385010a221d6e66a06ae450d7a98389e826141c3
TOMprojet <tomlecanu@gmail.com>
2024-04-14 18:48:46 +0200

    erqenrtdh

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import asyncio
 from database.db import fetch_user_configs
 from strategies.envelopes.strategie_a import execute_strategy_a_for_user
 from strategies.bolltrend.strategie_b import execute_strategy_b_for_user
-from utilities.bitget_perp import PerpBitget
+from utilities.bitget_perp import PerpBitget1
+from utilities.perp_bitget import PerpBitget2
 
 async def main():
     user_configs = fetch_user_configs()  # Assurez-vous que fetch_user_configs est une fonction asynchrone si nécessaire
 
     for account_config in user_configs:
         # Initialiser l'objet PerpBitget une seule fois par utilisateur
-        exchange = PerpBitget(
+        exchange1 = PerpBitget1(
+            public_api=account_config["public_key"],
+            secret_api=account_config["secret_key"],
+            password=account_config.get("password_api", ""),
+        )
+        exchange2 = PerpBitget2(
             public_api=account_config["public_key"],
             secret_api=account_config["secret_key"],
             password=account_config.get("password_api", ""),
@@ -17,12 +23,13 @@ async def main():
 
         # Exécuter la stratégie A ou B selon la configuration de l'utilisateur
         if account_config["strategy"] == "A":
-            await execute_strategy_a_for_user(account_config, exchange)
+            await execute_strategy_a_for_user(account_config, exchange1)
         elif account_config["strategy"] == "B":
-            await execute_strategy_b_for_user(account_config, exchange)
+            await execute_strategy_b_for_user(account_config, exchange2)
 
         # Assurez-vous de fermer l'échange après l'exécution de la stratégie
-        await exchange.close()
+        await exchange1.close()
+        await exchange2.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
