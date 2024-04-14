7853d67ee5b4cb70e362e25bcc900c7cb7a90872
TOMprojet <tomlecanu@gmail.com>
2024-04-14 10:56:04 +0200

    database modified

--- database/db.py
+++ database/db.py
@@ -1,7 +1,7 @@
 import boto3
 
 def fetch_user_configs():
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource('dynamodb', region_name ='eu-west-3')
     table = dynamodb.Table('Users')  # Remplacez 'Users' par le nom de votre table
     response = table.scan()
 
