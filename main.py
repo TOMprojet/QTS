1c63db2b863431341757ad1feaabc07b70951064
TOMprojet <tomlecanu@gmail.com>
2024-04-14 22:10:00 +0200

    TDFQTRB

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from utilities.bitget_perp import PerpBitget1
 from utilities.perp_bitget import PerpBitget2
 
 async def main():
-    user_configs = fetch_user_configs()  # Assurez-vous que fetch_user_configs est une fonction asynchrone si nécessaire
+    # Assurez-vous que fetch_user_configs est une fonction asynchrone si nécessaire
+    user_configs = await fetch_user_configs()
 
     for account_config in user_configs:
         # Initialiser l'objet PerpBitget une seule fois par utilisateur
@@ -22,12 +23,19 @@ async def main():
         )
 
         # Exécuter la stratégie A ou B selon la configuration de l'utilisateur
-        if account_config["strategy"] == "A":
-            await execute_strategy_a_for_user(account_config, exchange1)
-            await exchange1.close()
-        elif account_config["strategy"] == "B":
-            execute_strategy_b_for_user(account_config, exchange2)
+        try:
+            if account_config["strategy"] == "A":
+                await execute_strategy_a_for_user(account_config, exchange1)
+            elif account_config["strategy"] == "B":
+                await execute_strategy_b_for_user(account_config, exchange2)
+        finally:
+            # Assurez-vous de fermer les échanges après l'exécution de la stratégie
+            if 'close' in dir(exchange1):
+                await exchange1.close()
+            if 'close' in dir(exchange2):
+                await exchange2.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
