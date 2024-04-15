de5466072de48327bbc2e7e89612fb5d352bad93
TOMprojet <tomlecanu@gmail.com>
2024-04-15 21:22:57 +0200

    ergfdb

--- config/config_strat_b.py
+++ config/config_strat_b.py
@@ -1,142 +1,230 @@
-params2 = {
-        "BTC/USDT": {
-            "src": "close",
-            "ma_base_window": 7,
-            "envelopes": [0.07, 0.09, 0.12],
-            "size": 0.1,
-            "sides": ["long", "short"],
-        },
-        "ETH/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.07, 0.09, 0.12],
-            "size": 0.1,
-            "sides": ["long", "short"],
-        },
-        "ADA/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "AVAX/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "SOL/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "MATIC/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "XRP/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "LINK/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "LTC/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "ATOM/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "NEAR/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "ARB/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.1],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "TIA/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "ALGO/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "FIL/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "APT/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "INJ/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "SAND/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "TRX/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
-        "XTZ/USDT": {
-            "src": "close",
-            "ma_base_window": 5,
-            "envelopes": [0.09, 0.12, 0.15],
-            "size": 0.05,
-            "sides": ["long", "short"],
-        },
+params2= {
+    "BTC/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 2.25,
+        "long_ma_window": 500
+    },
+    "AAVE/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "APE/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "APT/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "AVAX/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "AXS/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "C98/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "CRV/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "DOGE/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "DOT/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "DYDX/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "ETH/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "FIL/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "FTM/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "BNB/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "GALA/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "GMT/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "GRT/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "KNC/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "KSM/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 2.25,
+        "long_ma_window": 500
+    },
+    "LRC/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "MANA/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "MASK/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "MATIC/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "NEAR/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "ONE/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "OP/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 2.25,
+        "long_ma_window": 500
+    },
+    "SAND/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "SHIB/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "SOL/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "STG/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "WAVES/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 2.25,
+        "long_ma_window": 500
+    },
+    "YFI/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "WOO/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 1,
+        "long_ma_window": 500
+    },
+    "EGLD/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 2.25,
+        "long_ma_window": 500
+    },
+    "ETC/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 2.25,
+        "long_ma_window": 500
+    },
+    "JASMY/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 2.25,
+        "long_ma_window": 500
+    },
+    "ROSE/USDT:USDT": {
+        "wallet_exposure": 0.05,
+        "bb_window": 100,
+        "bb_std": 2.25,
+        "long_ma_window": 500
+    },
 }
