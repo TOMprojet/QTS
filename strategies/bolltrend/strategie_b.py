85a74d011693cf96c76c83bc9761258e722f6568
TOMprojet <tomlecanu@gmail.com>
2024-04-20 12:25:31 +0200

    ty

--- strategies/bolltrend/strategie_b.py
+++ strategies/bolltrend/strategie_b.py
@@ -185,7 +185,7 @@ async def execute_strategy_b_for_user(account_config, exchange):
                 pct_exposition = (float(pos["contracts"]) * float(pos["contractSize"]) * float(pos["info"]["marketPrice"])) / usd_balance
                 positions_exposition[pos["pair"]]["long"] += pct_exposition
                 long_exposition += pct_exposition
-            elif pos["pair"] in df_list and pos["side"] == "short":
+            elif pos.pair in df_list and pos.side == "short":
                 pct_exposition = (float(pos["contracts"]) * float(pos["contractSize"]) * float(pos["info"]["marketPrice"])) / usd_balance
                 positions_exposition[pos["pair"]]["short"] += pct_exposition
                 short_exposition += pct_exposition
