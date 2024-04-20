f13c8d8fd99805bc64092660a41c15617cfe9793
TOMprojet <tomlecanu@gmail.com>
2024-04-20 16:14:56 +0200

    gjv

--- strategies/bolltrend/strategie_b.py
+++ strategies/bolltrend/strategie_b.py
@@ -157,7 +157,7 @@ async def execute_strategy_b_for_user(account_config, exchange):
                         pair, 
                         "sell", 
                         close_long_quantity,
-                        trigger_price=exchange.price_to_precision(pos.pair, pos.open_price * (1 - sl)),
+                        trigger_price=exchange.price_to_precision(pos["pair"], pos["open_price"] * (1 - sl)),
                         size=exchange.amount_to_precision(pos.pair, pos.size),
                         reduce=True)
                     positions_to_delete.append(pair)
