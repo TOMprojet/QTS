3dc1ef045af33efe7d4692bf0845fcda1d65b1e6
TOMprojet <tomlecanu@gmail.com>
2024-04-21 19:24:25 +0200

    gdjry

--- main.py
+++ main.py
@@ -5,35 +5,29 @@ from strategies.bolltrend.strategie_b import execute_strategy_b_for_user
 from utilities.bitget_perp import PerpBitget
 
 async def main():
-    # Récupérer les configurations utilisateur.
-    user_configs = await fetch_user_configs() 
-
-    # Créer une tâche pour chaque utilisateur et stratégie.
+    user_configs = await fetch_user_configs()
     tasks = []
+    exchanges = []
+
     for account_config in user_configs:
         exchange = PerpBitget(
             public_api=account_config["public_key"],
             secret_api=account_config["secret_key"],
             password=account_config.get("password_api", ""),
         )
-
         if account_config["strategy"] == "A":
-            task = execute_strategy_a_for_user(account_config, exchange)
+            tasks.append(execute_strategy_a_for_user(account_config, exchange))
         elif account_config["strategy"] == "B":
-            task = execute_strategy_b_for_user(account_config, exchange)
-        else:
-            continue  # Si aucune stratégie n'est définie, continuez avec le prochain utilisateur.
-        await exchange.close()
-        # Ajouter la coroutine de la tâche à la liste des tâches à exécuter.
-        tasks.append(task)
+            tasks.append(execute_strategy_b_for_user(account_config, exchange))
 
-    # Utiliser asyncio.gather pour exécuter toutes les tâches en parallèle.
+    # Exécutez toutes les tâches en parallèle
     await asyncio.gather(*tasks)
 
-    # Une fois toutes les tâches terminées, fermer les connexions de l'échange.
-    for task in tasks:
-        await task.exchange.close()
+    # Fermez toutes les instances d'échange après leur utilisation
+    for exchange in exchanges:
+        await exchange.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
