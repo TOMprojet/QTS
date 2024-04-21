01938e16bfebadce0fda91f26754655253d7830d
TOMprojet <tomlecanu@gmail.com>
2024-04-21 19:35:41 +0200

    zrcdc

--- database/db.py
+++ database/db.py
@@ -1,16 +1,8 @@
 import boto3
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
 
-# Fonction synchronisée pour récupérer les configurations utilisateur.
-def fetch_user_configs_sync():
-    dynamodb = boto3.resource('dynamodb', region_name='eu-west-3')
-    table = dynamodb.Table('Users') 
+def fetch_user_configs():
+    dynamodb = boto3.resource('dynamodb', region_name ='eu-west-3')
+    table = dynamodb.Table('Users')  # Remplacez 'Users' par le nom de votre table
     response = table.scan()
-    return response['Items']
 
-# Wrapper asynchrone autour de la fonction synchronisée.
-async def fetch_user_configs():
-    loop = asyncio.get_running_loop()
-    with ThreadPoolExecutor() as pool:
-        return await loop.run_in_executor(pool, fetch_user_configs_sync)
+    return response['Items']
--- main.py
+++ main.py
@@ -4,27 +4,25 @@ from strategies.envelopes.strategie_a import execute_strategy_a_for_user
 from strategies.bolltrend.strategie_b import execute_strategy_b_for_user
 from utilities.bitget_perp import PerpBitget
 
+
 async def main():
-    user_configs = await fetch_user_configs()
-    tasks = []
-    exchanges = []
+    # Assurez-vous que fetch_user_configs est une fonction asynchrone si nécessaire
+    user_configs = fetch_user_configs()
 
     for account_config in user_configs:
+        # Initialiser l'objet PerpBitget une seule fois par utilisateur
         exchange = PerpBitget(
             public_api=account_config["public_key"],
             secret_api=account_config["secret_key"],
             password=account_config.get("password_api", ""),
         )
+
+
         if account_config["strategy"] == "A":
-            tasks.append(execute_strategy_a_for_user(account_config, exchange))
-        elif account_config["strategy"] == "B":
-            tasks.append(execute_strategy_b_for_user(account_config, exchange))
+            await execute_strategy_a_for_user(account_config, exchange)
+        if account_config["strategy"] == "B":
+            await execute_strategy_b_for_user(account_config, exchange)
 
-    # Exécutez toutes les tâches en parallèle
-    await asyncio.gather(*tasks)
-
-    # Fermez toutes les instances d'échange après leur utilisation
-    for exchange in exchanges:
         await exchange.close()
 
 if __name__ == "__main__":
