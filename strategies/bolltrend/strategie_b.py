aa2735c0073339753ae8b737868c6cd6ab86f57f
TOMprojet <tomlecanu@gmail.com>
2024-04-21 11:01:39 +0200

    edfge

--- strategies/bolltrend/strategie_b.py
+++ strategies/bolltrend/strategie_b.py
@@ -111,7 +111,10 @@ async def execute_strategy_b_for_user(account_config, exchange):
             df['iloc'] = range(len(df))
 
         print("Indicators loaded 100%")
-
+        print(df["n1_higher_band"])
+        print(df["n1_lower_band"])
+        print(df['long_ma'])
+        
         var = ValueAtRisk(df_list=df_list.copy())
         var.update_cov(current_date=df_list["BTC/USDT"].index[-1], occurance_data=989)
         print("Value At Risk loaded 100%")
