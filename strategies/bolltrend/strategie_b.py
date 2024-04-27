70a415727b2374b8969e41fd4534f437147bdcdf
TOMprojet <tomlecanu@gmail.com>
2024-04-27 14:01:17 +0200

    jvklc

--- strategies/bolltrend/strategie_b.py
+++ strategies/bolltrend/strategie_b.py
@@ -238,7 +238,7 @@ async def execute_strategy_b_for_user(account_config, exchange):
                                 f"Place Open Long Market Order: {long_quantity} {pair[:-5]} at the price of {long_market_price}$ ~{round(exchange_long_quantity, 2)}$"
                             )
                             if production:
-                                exchange.place_order(pair, "buy", long_quantity, reduce=False)
+                                exchange.place_order(pair, "buy", long_quantity, size=exchange.amount_to_precision(position.pair, position.size), reduce=False)
                                 positions_exposition[pair]["long"] += (long_quantity_in_usd / usd_balance)
                                 long_exposition += (long_quantity_in_usd / usd_balance)
 
@@ -260,7 +260,7 @@ async def execute_strategy_b_for_user(account_config, exchange):
                                 f"Place Open Short Market Order: {short_quantity} {pair[:-5]} at the price of {short_market_price}$ ~{round(exchange_short_quantity, 2)}$"
                             )
                             if production:
-                                exchange.place_order(pair, "sell", short_quantity, reduce=False)
+                                exchange.place_order(pair, "sell", short_quantity, size=exchange.amount_to_precision(position.pair, position.size), reduce=False)
                                 positions_exposition[pair]["short"] += (short_quantity_in_usd / usd_balance)
                                 short_exposition += (short_quantity_in_usd / usd_balance)
                     
